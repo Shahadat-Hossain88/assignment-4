2fbd488906563a7e36b6b6d31a39db22df7ec5a7
Shahadat <mdshahadat1802@gmail.com>
2021-01-30 17:01:13 +0600

    second

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function ticketBooking(className,isIncrese){
-    const ticketInput = document.getElementById(className+'-ticket-count');
+function ticketBooking(className, isIncrese) {
+    const ticketInput = document.getElementById(className + '-ticket-count');
     const ticketCount = parseInt(ticketInput.value);
     let ticketNewCount = ticketCount;
     if (isIncrese == true) {
@@ -10,32 +10,47 @@ function ticketBooking(className,isIncrese){
     }
     ticketInput.value = ticketNewCount;
     let ticketAmount = 0;
-    if(className == 'firstclass'){
+    if (className == 'firstclass') {
         ticketAmount = ticketNewCount * 150;
-        
+
     }
-    if(className == 'economy'){
+    if (className == 'economy') {
         ticketAmount = ticketNewCount * 100;
-       
+
     }
     totalCalculate();
 }
 
-function totalCalculate(){
+function totalCalculate() {
     const firstclassTicketCount = takeValue('firstclass')
     const economyTicketCount = takeValue('economy')
-    let totalAmount = firstclassTicketCount * 150 +  economyTicketCount* 100  ;
-    document.getElementById('total-amount').innerText = '$'+ totalAmount;
+    let totalAmount = firstclassTicketCount * 150 + economyTicketCount * 100;
+    document.getElementById('total-amount').innerText = '$' + totalAmount;
     const vat = Math.round(totalAmount * 0.1);
-    document.getElementById('vat').innerText = '$'+vat;
+    document.getElementById('vat').innerText = '$' + vat;
 
-    totalCost = totalAmount - vat;
-    document.getElementById('total-cost').innerText = '$'+totalCost ;
+    totalCost = totalAmount + vat;
+    document.getElementById('total-cost').innerText = '$' + totalCost;
 
 
 }
-function takeValue(className){
-    const ticketInput = document.getElementById(className+'-ticket-count');
+function takeValue(className) {
+    const ticketInput = document.getElementById(className + '-ticket-count');
     const ticketCount = parseInt(ticketInput.value);
     return ticketCount;
 }
+let bookBtn = document.getElementById("book-now");
+bookBtn.addEventListener('click', function () {
+    const bookArea = document.getElementById("book-area");
+    bookArea.style.display = 'none ';
+})
+const bookBtnArea = document.getElementById("book-now");
+bookBtnArea.addEventListener('click', function () {
+    const bookArea = document.getElementById("book-btn-area");
+    bookArea.style.display = 'none';
+})
+bookBtn = document.getElementById("book-now");
+bookBtnArea.addEventListener('click', function () {
+    const bookArea = document.getElementById("output-area");
+    bookArea.style.display = 'block';
+})
